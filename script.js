aebadf6ed000222ae197e6817e98feee4123ca01
Luís Clapp <fhelipebarros@hotmail.com>
2022-07-13 10:51:06 -0300

    changes in script and index

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function calculate() {
     let hours = document.getElementById('perDay').value;
-    console.log(hours);
+    
     let age = document.getElementById('age').value;
-    console.log(age);
+    
     let result = 80 - age;
-    console.log(result);
-    let sleepYears = (result * hours) / 24;
-    console.log(sleepYears)
-    return document.getElementById("result").innerHTML =(sleepYears.toFixed(2) + " anos");
+    
+    let sleepYear = (result * hours) / 24;
+
+    return document.getElementById("result").innerHTML =(sleepYear.toFixed(2) + " anos");
 }
